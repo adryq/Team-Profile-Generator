caeabda2a35b0d15067b3ec7d1373dcfdc45d77a
Adry Quezada <adryq95@gmail.com>
2022-09-21 22:46:54 -0400

    added intern tests

--- __tests__/Intern.test.js
+++ __tests__/Intern.test.js
@@ -1,1 +1,65 @@
 const Intern = require('../lib/intern');
+
+
+test('if created intern object', () => {
+   
+    const name = 'Adry'
+    const eid = '9'
+    const email = 'adryq95@gmail.com'
+    const school = 'Columbia'
+    
+    const intern = new Intern(name, eid, email, school)
+
+    expect(intern.name).toEqual('Adry');
+    expect(intern.eid).toEqual('9');
+    expect(intern.email).toEqual('adryq95@gmail.com');
+    expect(intern.school).toEqual('Columbia');
+    
+});
+
+test('get name', () => {
+
+    const intern = new Intern('Adry', '9', 'adryq95@gmail.com', 'Columbia');
+
+    expect(intern.getName()).toEqual('Adry');
+
+})
+
+test('get intern id', () => {
+
+    const intern = new Intern('Adry', '9', 'adryq95@gmail.com', 'Columbia');
+
+    expect(intern.getEid()).toEqual('9');
+
+})
+
+test('get email', () => {
+
+    const intern = new Intern('Adry', '9', 'adryq95@gmail.com', 'Columbia');
+
+    expect(intern.getEmail()).toEqual('adryq95@gmail.com');
+
+})
+
+test('get school ', () => {
+
+    const intern = new Intern('Adry', '9', 'adryq95@gmail.com', 'Columbia');
+
+    expect(intern.getSchool()).toEqual('Columbia');
+
+})
+
+test('get role returning intern', () => {
+
+    const name = 'Adry'
+    const eid = '9'
+    const email = 'adryq95@gmail.com'
+    const school = 'Columbia'
+
+    const intern = new Intern(name, eid, email, school)
+
+    const Role = intern.getRole();
+
+    expect('Intern').toEqual(Role)
+
+})
